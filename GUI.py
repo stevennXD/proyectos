f5b2dd49da321ef1d2359f10006667be59b8265e
22B03-PC\22B <bryan.perez01@epn.edu.ec>
2017-05-17 12:47:04 -0500

    Primera Version

---
+++ GUI.py
@@ -0,0 +1,9 @@
+Python 3.5.0 (v3.5.0:374f501f4567, Sep 13 2015, 02:27:37) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> from tkinter import*
+>>> tk = Tk()
+>>> btn = Button(tk, text = "Click me")
+>>> btn.pack
+<bound method Pack.pack_configure of <tkinter.Button object .54899712>>
+>>> btn.pack()
+>>> 
